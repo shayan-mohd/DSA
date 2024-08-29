8439680c1ffdbf62db251da9adad84bf677dea40
Mohd Shayan Ansari <shayanah2011@gmail.com>
2024-08-29 15:11:54 +0530

    Moore's algo

---
+++ src/Arrays/Majority_Element_169.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Majority_Element_169 {
+    public int majorityElement(int[] nums) {
+        int res = 0, count = 0;
+
+        for (int x: nums) {
+            if (count == 0) {
+                res = x;
+            }
+
+            if (res != x) {
+                count--;
+            }
+            else {
+                count++;
+            }
+        }
+        return res;
+    }
+}
